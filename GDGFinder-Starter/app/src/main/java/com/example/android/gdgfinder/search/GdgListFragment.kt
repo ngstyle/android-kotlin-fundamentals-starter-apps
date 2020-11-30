d65e2259303db56cff11c04e61edd31f2aa8783c
chon den <379903688@qq.com>
2020-11-30 16:04:53 +0800

    Material Design, dimens, and colors

--- GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
+++ GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
@@ -52,7 +52,7 @@ class GdgListFragment : Fragment() {
         val binding = FragmentGdgListBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
-        binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = this
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
